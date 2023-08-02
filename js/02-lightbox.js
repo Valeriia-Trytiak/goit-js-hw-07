40bd62bfc53c44f9b34df90deb33fdfdb4b6baa6
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-08-02 14:33:50 +0200

    update task-gallery-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+/*
+1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+2. Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+3. Инициализация библиотеки после того как элементы галереи созданы и добавлены в ul.gallery. Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+4. Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.*/
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+// Создание разметки
+function creatGalleryEl(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>
+          `
+    )
+    .join("");
+}
+
+// Добавление в ДОМ
+galleryContainer.insertAdjacentHTML("beforeend", creatGalleryEl(galleryItems));
+// console.log(galleryItems);
