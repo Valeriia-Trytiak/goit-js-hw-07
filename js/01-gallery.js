12c63bd1ae039cf485cbcecb3372b5b3278e9978
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-08-02 13:03:23 +0200

    task-gallery-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+/* 
+1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+2. Реализация делегирования на ul.gallery и получение url большого изображения.
+3. Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+4. Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+5. Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.*/
+
+const galleryContainer = document.querySelector(".gallery");
+
+// Создание разметки
+function creatGalleryEl(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="I${description}"
+          />
+        </a>
+      </li>
+        `
+    )
+    .join("");
+}
+
+// Добавление в ДОМ
+galleryContainer.insertAdjacentHTML("beforeend", creatGalleryEl(galleryItems));
+
+// Делегирование получение ссылки на большое изображение
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+  const linkGalleryEl = document.querySelector(".gallery__link");
+  if (evt.target === linkGalleryEl) {
+    evt.target.preventDefault();
+    return;
+  }
+
+  const oringinalImgSrc = evt.target.dataset.source;
+  //   console.log(oringinalImgSrc);
+  //   console.log(`єто таргет`, evt.target);
+  const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${oringinalImgSrc}" width="1200">
+    </div>
+`);
+
+  instance.show();
+}
+
+// console.log(galleryItems);
